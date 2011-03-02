bd40ae009e1f4cab3dc5c843acf035fc04ab05f6
james-mountford <james-mountford@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-02 12:35:09 +0000

    git-svn-id: https://plugins.svn.wordpress.org/showhide-admin-bar-in-wp31/tags/1.1@354142 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,22 @@
+﻿<?php
+/*
+Plugin Name: Show/Hide WordPress Admin Bar
+Plugin URI: http://www.sitepower.co.uk/
+Description: Shows/Hides the WP Admin Bar
+Author: James Mountford
+Version: 1.1
+Author URI: http://www.sitepower.co.uk/
+*/
+require(ABSPATH . WPINC . '/pluggable.php');
+global $current_user;
+get_currentuserinfo();
+if($current_user->ID){
+//if(is_user_logged_in()){
+	add_action('wp_head', 'fix_admin_bar');
+//}
+}
+function fix_admin_bar(){
+	echo "<script type='text/javascript' src='", get_bloginfo('wpurl'), "/wp-content/plugins/showhide-admin-bar-in-wp31/js/show-hide-admin-bar.js'></script>";
+}
+
+?>
